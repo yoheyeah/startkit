a933700483198e6e0cdbb74c5cae9a98bcb4c3bb
yoyoyeah <harrisin2037@gmail.com>
2019-03-16 01:40:44 +0800

    cors Authorization header

--- library/gins/middleware.go
+++ library/gins/middleware.go
@@ -75,7 +75,7 @@ func CORS() gin.HandlerFunc {
 		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Content-Disposition")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Content-Disposition, Authorization")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusOK)
 		}
